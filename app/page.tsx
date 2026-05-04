2dc30e95261d7cb86a0f79deb49533510b364bae
Ajay Kumar Lakshmpura Vijaykumar <78868930+AjayKumar-LVLS@users.noreply.github.com>
2026-05-03 21:33:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { HeroSection } from "@/components/hero-section"
 import { AboutSection } from "@/components/about-section"
 import { ExperienceSection } from "@/components/experience-section"
